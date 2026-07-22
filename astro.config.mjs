d8e6dcda788e62fc538e779d166dca13a79511b7
marcelo <marcelo.caldas@fpf.br>
2026-07-22 12:22:25 -0400

    fix: set root base path for custom domain marcelocaldasdevops.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marcelodosanjosc.github.io',
-  base: '/marcelodosanjoscsite/',
+  site: 'https://marcelocaldasdevops.com',
+  base: '/',
   build: {
     format: 'file'
   }
